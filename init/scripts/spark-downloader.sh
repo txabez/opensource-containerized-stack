6800ed7297235f1db68716d2655f823a258f59f6
Xabier Gil <xabier.gil@gmail.com>
2024-11-12 17:06:44 +0100

    first containers for local stack

---
+++ init/scripts/spark-downloader.sh
@@ -0,0 +1,12 @@
+export SPARK_VERSION=3.5.3
+export HADOOP_VERSION=hadoop3
+export INSTALL_ROOT=/downloads
+
+if [ ! -d "${INSTALL_ROOT}/spark-${SPARK_VERSION}-bin-${HADOOP_VERSION}" ];
+then
+	echo "Installing Spark 'spark-${SPARK_VERSION}-bin-${HADOOP_VERSION}'"
+  mkdir -p "${INSTALL_ROOT}" && cd "${INSTALL_ROOT}" && \
+    wget --show-progress https://archive.apache.org/dist/spark/spark-${SPARK_VERSION}/spark-${SPARK_VERSION}-bin-${HADOOP_VERSION}.tgz && \
+    tar -xzf spark-${SPARK_VERSION}-bin-${HADOOP_VERSION}.tgz
+fi
+echo "Spark 'spark-${SPARK_VERSION}-bin-${HADOOP_VERSION}' already installed"
